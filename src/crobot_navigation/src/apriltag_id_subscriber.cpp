cb7993259c78b6f74b15119f98ede3477eb927a9
Olivia <msoliviawatt@gmail.com>
2025-03-29 02:36:42 -0400

    um

--- src/crobot_navigation/src/apriltag_id_subscriber.cpp
+++ src/crobot_navigation/src/apriltag_id_subscriber.cpp
@@ -9,13 +9,26 @@ AprilTagSubscriberID::AprilTagSubscriberID(const std::string &name, const BT::No
 AprilTagSubscriberID::~AprilTagSubscriberID() {
 }
 
+NavGoal MakeNavGoal(float x, float y, float th)
+{
+    auto navGoal = NavPose::Goal();
+    navGoal.pose.header.frame_id = "map";
+    navGoal.pose.pose.position.x = x;
+    navGoal.pose.pose.position.y = y;
+
+    tf2::Quaternion q;
+    q.setRPY(0, 0, th);
+    navGoal.pose.pose.orientation = tf2::toMsg(q);
+
+    return navGoal;
+}
+
 void AprilTagSubscriberID::preparePoints() {
-    // TODO: Set april tag points here
-    positions.push_back({10, 215, 0});
-    positions.push_back({33, 215, 0});
-    positions.push_back({56, 215, 0});
-    positions.push_back({79, 215, 0});
-    positions.push_back({102, 215, 0});
+    positions.push_back{MakeNavGoal(10, 215, 0)};
+    positions.push_back{MakeNavGoal(33, 215, 0)};
+    positions.push_back{MakeNavGoal(56, 215, 0)};
+    positions.push_back{MakeNavGoal(79, 215, 0)};
+    positions.push_back{MakeNavGoal(102, 215, 0)};
 }
 
 BT::NodeStatus AprilTagSubscriberID::onStart() {
