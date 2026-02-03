62b80b3f50c30bba4d43b93bca72ee95b9e4ce11
Olivia Watt <msoliviawatt@gmail.com>
2026-02-02 20:09:06 -0500

    documentation for dorne

--- src/crobot_drone_controller/crobot_drone_controller/takeoff_and_hover.py
+++ src/crobot_drone_controller/crobot_drone_controller/takeoff_and_hover.py
@@ -3,22 +3,36 @@ import time
 import rclpy
 from rclpy.node import Node
 
-from mavros_msgs.msg import State
+from mavros_msgs.msg import State # mavros_msgs/State is the ROS message that MAVROS publishes
+
+# TOL = takeoff and land:o
 from mavros_msgs.srv import CommandBool, CommandTOL, SetMode
 
 class TakeoffAndHover(Node):
     def __init__(self):
+        # creates the ros node with the name 'takeoff_and_hover'
         super().__init__('takeoff_and_hover')
 
-        self.declare_parameter('takeoff_altitude', 1.5)
-        self.declare_parameter('mode', 'GUIDED')
-        self.declare_parameter('connect_timeout_s', 15.0)
 
+        # you can change these parameters when you run the node
+        # e.g. ros2 run ... --ros-args -p takeoff_altitude:=2.0 -p mode:=GUIDED_NOGPS
+        self.declare_parameter('takeoff_altitude', 1.5) # altitude is in meters
+        self.declare_parameter('mode', 'GUIDED_NOGPS') # we don't have gps
+        self.declare_parameter('connect_timeout_s', 15.0) # after 15 seconds, we will stop waiting to connect
+
+        # check state stuff: https://docs.ros.org/en/api/mavros_msgs/html/msg/State.html
         self.state = State()
 
+        # subscriber
         self.create_subscription(State, '/mavros/state', self._state_cb, 10)
+
+        # calling mavros to change flight mode
         self.set_mode_cli = self.create_client(SetMode, '/mavros/set_mode')
+
+        # calling mavros to arm or disarm so that the flight controller can use tje motors
         self.arm_cli = self.create_client(CommandBool, '/mavros/cmd/arming')
+
+        # calls mavros, to command that we take off, given an altitude
         self.takeoff_cli = self.create_client(CommandTOL, '/mavros/cmd/takeoff')
 
     def _state_cb(self, msg):
@@ -39,26 +53,26 @@ class TakeoffAndHover(Node):
         self.get_logger().info("Connected.")
 
         # guided mode
-        req = SetMode.Request()
-        req.base_mode = 0
-        req.custom_mode = self.get_parameter('mode').value
-        self.set_mode_cli.call_async(req)
+        request = SetMode.Request()
+        request.base_mode = 0
+        request.custom_mode = self.get_parameter('mode').value
+        self.set_mode_cli.call_async(request)
         time.sleep(2)
 
         # arm
-        req = CommandBool.Request()
-        req.value = True
-        self.arm_cli.call_async(req)
+        request = CommandBool.Request()
+        request.value = True
+        self.arm_cli.call_async(request)
         time.sleep(2)
 
         # takeoff
-        req = CommandTOL.Request()
-        req.altitude = float(self.get_parameter('takeoff_altitude').value)
-        req.latitude = 0.0
-        req.longitude = 0.0
-        req.min_pitch = 0.0
-        req.yaw = 0.0
-        self.takeoff_cli.call_async(req)
+        request = CommandTOL.Request()
+        request.altitude = float(self.get_parameter('takeoff_altitude').value)
+        request.latitude = 0.0
+        request.longitude = 0.0
+        request.min_pitch = 0.0
+        request.yaw = 0.0
+        self.takeoff_cli.call_async(request)
 
         self.get_logger().info("Takeoff command sent.")
 
@@ -67,10 +81,15 @@ class TakeoffAndHover(Node):
             
 
 def main():
+    # ros!
     rclpy.init()
+
+    # create, run, delete node
     node = TakeoffAndHover()
     node.run()
     node.destroy_node()
+
+    # byebye, ros :(
     rclpy.shutdown()
 
 if __name__ == '__main__':
