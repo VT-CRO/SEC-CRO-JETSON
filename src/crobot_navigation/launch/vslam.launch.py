fd7c71ca76c0d360724d3350d8d7be81a1029aaf
Olivia Watt <msoliviawatt@gmail.com>
2026-02-17 18:25:09 -0500

    nav2, nvblox launch files

--- src/crobot_navigation/launch/vslam.launch.py
+++ src/crobot_navigation/launch/vslam.launch.py
@@ -13,10 +13,10 @@ def generate_launch_description():
         parameters=[{
             'enable_infra1': True,
             'enable_infra2': True,
-            'enable_color': False,
-            'enable_depth': False,
+            'enable_color': True,
+            'enable_depth': True,
             'depth_module.emitter_enabled': 0,
-            'depth_module.profile': '640x360x90',
+            'depth_module.profile': '640x360x60',
             'enable_gyro': True,
             'enable_accel': True,
             'gyro_fps': 200,
