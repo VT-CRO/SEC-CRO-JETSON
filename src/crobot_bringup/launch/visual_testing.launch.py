c6efba6896376745d0cea65b66040b6c326ba7e9
advancecuser2005 <mailtodaksh@vt.edu>
2026-02-23 00:36:02 +0000

    Transformed from OnExecutionComplete to Delay Timer

--- src/crobot_bringup/launch/visual_testing.launch.py
+++ src/crobot_bringup/launch/visual_testing.launch.py
@@ -1,18 +1,15 @@
-# main_launch.py
 from launch import LaunchDescription
 from launch.actions import (
     IncludeLaunchDescription,
-    RegisterEventHandler,
-    LogInfo
+    LogInfo,
+    TimerAction
 )
-from launch.event_handlers import OnProcessExit, OnExecutionComplete
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.substitutions import FindPackageShare
 from launch.substitutions import PathJoinSubstitution
 
 
 def generate_launch_description():
-
 
     bringup_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
@@ -44,29 +41,25 @@ def generate_launch_description():
         ])
     )
 
-    start_vslam_after_bringup = RegisterEventHandler(
-        OnExecutionComplete(
-            target_action=bringup_launch,
-            on_completion=[
-                LogInfo(msg='Bringup finished! Starting VSLAM...'),
-                vslam_launch,
-            ]
-        )
+    delayed_vslam = TimerAction(
+        period=5.0,      
+        actions=[
+            LogInfo(msg='Starting VSLAM...'),
+            vslam_launch,
+        ]
     )
 
-    start_nvblox_after_vslam = RegisterEventHandler(
-        OnExecutionComplete(
-            target_action=vslam_launch,
-            on_completion=[
-                LogInfo(msg='VSLAM finished! Starting NVBLOX...'),
-                nvblox_launch,
-            ]
-        )
+    delayed_nvblox = TimerAction(
+        period=10.0,     
+        actions=[
+            LogInfo(msg='Starting NVBLOX...'),
+            nvblox_launch,
+        ]
     )
 
     return LaunchDescription([
         LogInfo(msg='Starting Bringup...'),
         bringup_launch,
-        start_vslam_after_bringup,
-        start_nvblox_after_vslam
+        delayed_vslam,
+        delayed_nvblox,
     ])
