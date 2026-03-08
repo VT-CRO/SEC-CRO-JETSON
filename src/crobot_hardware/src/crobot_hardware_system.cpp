cc05d695d71a904547fec1fd7e50465a77a30491
Evan Goenner <52749520+vangohner@users.noreply.github.com>
2026-03-08 00:46:44 +0000

    set hardware conversion factor 1

--- src/crobot_hardware/src/crobot_hardware_system.cpp
+++ src/crobot_hardware/src/crobot_hardware_system.cpp
@@ -337,11 +337,12 @@ namespace crobot_hardware
         j["cmd"] = "write";
 
         const double RAD_TO_DEG = 180.0 / M_PI;
+        const int hardware_conversion_factor = 1.0;
 
-        j["ankles"]["front_left"] = (int)(130.0 + ankles_[0].cmd * RAD_TO_DEG / 0.84);
-        j["ankles"]["front_right"] = (int)(53.0 + ankles_[1].cmd * RAD_TO_DEG / 0.84);
-        j["ankles"]["back_left"] = (int)(57.0 + ankles_[2].cmd * RAD_TO_DEG / 0.84);
-        j["ankles"]["back_right"] = (int)(110.0 + ankles_[3].cmd * RAD_TO_DEG / 0.84);
+        j["ankles"]["front_left"] = (int)(130.0 + ankles_[0].cmd * RAD_TO_DEG / hardware_conversion_factor);
+        j["ankles"]["front_right"] = (int)(53.0 + ankles_[1].cmd * RAD_TO_DEG / hardware_conversion_factor);
+        j["ankles"]["back_left"] = (int)(57.0 + ankles_[2].cmd * RAD_TO_DEG / hardware_conversion_factor);
+        j["ankles"]["back_right"] = (int)(110.0 + ankles_[3].cmd * RAD_TO_DEG / hardware_conversion_factor);
 
         const double MAX_WHEEL_SPEED = cfg_.max_wheel_speed_meters / cfg_.wheel_radius;  // r/s corresponding to full command (255)
 
